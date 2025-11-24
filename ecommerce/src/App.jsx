2792c0b84a4dafa178c8618a7592143f0e31c498
Siddhant Sonarkar <siddhantsonarkar9@gmail.com>
2025-11-24 10:39:49 +0530

    move all pages to react

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,14 +1,18 @@
 import { HomePage } from './pages/HomePage'
 import {Routes, Route} from 'react-router';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
 import './App.css'
+import { TrackingPage } from './pages/TrackingPage';
 
 function App() {
   
   return (
    <Routes>
-    <Route path='/' element={<HomePage/> }></Route>
+    <Route index element={<HomePage/> }></Route>
     <Route path='checkout' element={<CheckoutPage/>}></Route>
+    <Route path='orders' element={<OrdersPage></OrdersPage>}></Route>
+    <Route path='tracking' element={<TrackingPage/>}></Route>
     
    </Routes>
      
