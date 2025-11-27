ffc38fc9ea9ffdad98ac3ff6d02d36f83c574abf
Siddhant Sonarkar <siddhantsonarkar9@gmail.com>
2025-11-27 22:07:20 +0530

    fetch data

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -5,6 +5,12 @@ import { products } from '../../starting-code/data/products';
 
 
 export function HomePage(){
+    fetch("http://localhost:3000/api/products")
+     .then((response)=>{
+        response.json().then((data)=>{
+            console.log(data);
+        })
+     });
     return(
         <>
         <title>UrbanCart</title>
